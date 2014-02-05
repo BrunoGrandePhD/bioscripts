19b87a80cb4a74ed6827dedccf0ae4e0527a6266
Bruno Grande <contact@brunogrande.me>
2014-02-05 10:37:42 -0800

    Improved file naming with find_heterozygous_positions.py

--- find_heterozygous_positions.py
+++ find_heterozygous_positions.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-# This script takes one argument, a SNVMix2 output file. 
+# This script takes one argument, a SNVMix2 output file.
 # This script will read each line of the SNVMix2 output
 # and filter it down to positions that have a >0.99
-# probability of being heterozygous. 
+# probability of being heterozygous.
 # The output of this script is a .POS file, which is a
 # tab-separated format with two columns, chromosome and
-# position. This output file will be saved alongside 
-# the input SNVMix file. 
+# position. This output file will be saved alongside
+# the input SNVMix file.
 
 import sys
 import os
@@ -16,7 +16,7 @@ inputFilePath = sys.argv[1]
 inputFileDir, inputFileName = os.path.split(inputFilePath)
 inputFile = open(inputFilePath, "r")
 # Empty output file
-open(inputFileDir + "/" + os.path.splitext(inputFileName)[0] + ".pos", "w").close()
+open(inputFileDir + "/" + os.path.splitext(inputFileName)[0] + ".filt_AB.pos", "w").close()
 outputFile = open(inputFileDir + "/" + os.path.splitext(inputFileName)[0] + ".pos", "a")
 
 for line in inputFile:
