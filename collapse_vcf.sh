c7e56c27f9a1f168fecf11fcd549f9ecd3c8620f
Bruno Grande <contact@brunogrande.me>
2014-03-28 12:42:25 -0700

    Fixed bug with collapse_vcf where the headers were added 3 times

--- collapse_vcf.sh
+++ collapse_vcf.sh
@@ -8,7 +8,7 @@ cat $1 | vcf-sort | bgzip -c > $1.gz ; tabix -p vcf $1.gz
 cat $2 | vcf-sort | bgzip -c > $2.gz ; tabix -p vcf $2.gz
 
 vcf-isec -c $1.gz $2.gz > $file1_path/collapse_vcf.$file1_name.and.$file2_name
-vcf-isec -c $2.gz $1.gz >> $file1_path/collapse_vcf.$file1_name.and.$file2_name
-vcf-isec -n =2 $1.gz $2.gz >> $file1_path/collapse_vcf.$file1_name.and.$file2_name
+vcf-isec -c $2.gz $1.gz | egrep -v '^#' >> $file1_path/collapse_vcf.$file1_name.and.$file2_name
+vcf-isec -n =2 $1.gz $2.gz | egrep -v '^#' >> $file1_path/collapse_vcf.$file1_name.and.$file2_name
 
 rm $1.gz $1.gz.tbi $2.gz $2.gz.tbi
